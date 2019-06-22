425e4ad84321a70e1b538e4f0819b4ceb3293bce
OscarFabiani <spartan539@yahoo.com>
2019-06-21 18:02:24 -0700

    Added index.js.

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var express = require('express');
+var multer = require('multer');
+var upload = multer();
+
+
+var app = express();
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+
+
+app.route('/api/fileanalyse')
+.post(upload.single('upfile'), function(req, res) {
+  var file = req.file
+  console.log(file);
+  if (!file) { res.json({ error: 'file required' }) }
+  else {
+    res.json({
+      name: file.originalname,
+      type: file.mimetype,
+      size: file.size
+    });
+  };
+});
+
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
